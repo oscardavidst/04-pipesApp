d1b029a35f678f7ab2fde02d5366e9b31b90fee5
Oscar Soto <oscarqpzm95@gmail.com>
2023-02-12 21:24:21 -0500

    Configuracion del RouterModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRouterModule } from './app-router.module';
 
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, CommonModule, SharedModule],
+  imports: [BrowserModule, AppRouterModule, CommonModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
 })
