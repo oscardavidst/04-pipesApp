3858a770490f7f9d172b8ba31ec8e7f334ac8faa
Oscar Soto <oscarqpzm95@gmail.com>
2023-02-12 19:01:00 -0500

    Configuración del proyecto con PrimeNg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+// Modulo personalizado de PrimeNg
+import { PrimeNgModule } from './prime-ng/prime-ng.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, CommonModule, PrimeNgModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/prime-ng/prime-ng.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+// PrimeNg
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+
+@NgModule({
+  declarations: [],
+  exports: [ButtonModule, CardModule],
+})
+export class PrimeNgModule {}
