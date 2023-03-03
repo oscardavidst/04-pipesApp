7f4c974adab82ccf0ab9f0b59639d0a2c8396600
Oscar Soto <oscarqpzm95@gmail.com>
2023-03-02 22:43:55 -0500

    Cambiar el locale de la aplicación

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRouterModule } from './app-router.module';
 
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { VentasModule } from './ventas/ventas.module';
+
+// Cambiar el locale de la app
+import localeEsCo from '@angular/common/locales/es-CO';
+import { registerLocaleData } from '@angular/common';
+registerLocaleData(localeEsCo);
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,7 +21,7 @@ import { VentasModule } from './ventas/ventas.module';
     SharedModule,
     VentasModule,
   ],
-  providers: [],
+  providers: [{ provide: LOCALE_ID, useValue: 'es-CO' }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
