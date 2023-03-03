f496b01ede4bcacdc421f7320e4709d3649e30a2
Oscar Soto <oscarqpzm95@gmail.com>
2023-03-02 22:28:03 -0500

    Pipes: Uppercase, Lowercase, tittlecase y Date

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,17 @@ import { AppRouterModule } from './app-router.module';
 
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { VentasModule } from './ventas/ventas.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRouterModule, CommonModule, SharedModule],
+  imports: [
+    BrowserModule,
+    AppRouterModule,
+    CommonModule,
+    SharedModule,
+    VentasModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/ventas/pages/basicos/basicos.component.ts
+++ src/app/ventas/pages/basicos/basicos.component.ts
@@ -3,9 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-basicos',
   templateUrl: './basicos.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class BasicosComponent {
+  nombreLower: string = 'oscar';
+  nombreUpper: string = 'OSCAR';
+  nombreCompleto: string = 'osCaR';
 
+  fecha: Date = new Date();
 }
