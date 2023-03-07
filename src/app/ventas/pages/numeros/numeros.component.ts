20d86a46f2b1bf9f04cf9de3162d1a93ed40e3e3
Oscar Soto <oscarqpzm95@gmail.com>
2023-03-06 19:36:01 -0500

    Decimal Pipe

--- src/app/ventas/pages/numeros/numeros.component.ts
+++ src/app/ventas/pages/numeros/numeros.component.ts
@@ -3,9 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-numeros',
   templateUrl: './numeros.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class NumerosComponent {
-
+  ventasNetas: number = 4400000.8736;
+  porcentaje: number = 0.48;
 }
