5d68a269556a46dce82797b97db57e2bec9e8317
Oscar Soto <oscarqpzm95@gmail.com>
2023-03-08 13:12:06 -0500

    PrimeTable y PrimeToolbar

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -5,9 +5,20 @@ import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { FieldsetModule } from 'primeng/fieldset';
 import { MenubarModule } from 'primeng/menubar';
+import { TableModule } from 'primeng/table';
+import { ToolbarModule } from 'primeng/toolbar';
+import { SplitButtonModule } from 'primeng/splitbutton';
 
 @NgModule({
   declarations: [],
-  exports: [ButtonModule, CardModule, FieldsetModule, MenubarModule],
+  exports: [
+    ButtonModule,
+    CardModule,
+    FieldsetModule,
+    MenubarModule,
+    TableModule,
+    ToolbarModule,
+    SplitButtonModule,
+  ],
 })
 export class PrimeNgModule {}
