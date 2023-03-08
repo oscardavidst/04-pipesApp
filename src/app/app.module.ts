7a8b5f68964ee3d265882f64ebda88f14e262576
Oscar Soto <oscarqpzm95@gmail.com>
2023-03-07 19:43:47 -0500

    PrimeNg - Fieldset

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRouterModule } from './app-router.module';
 
 import { AppComponent } from './app.component';
@@ -18,6 +19,7 @@ registerLocaleData(localeFr);
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRouterModule,
     CommonModule,
     SharedModule,
--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 // PrimeNg
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
+import { FieldsetModule } from 'primeng/fieldset';
 import { MenubarModule } from 'primeng/menubar';
 
 @NgModule({
   declarations: [],
-  exports: [ButtonModule, CardModule, MenubarModule],
+  exports: [ButtonModule, CardModule, FieldsetModule, MenubarModule],
 })
 export class PrimeNgModule {}
