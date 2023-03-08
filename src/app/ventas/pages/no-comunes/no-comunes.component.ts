201d3294a317e07ed6d74560af693788c1ab71cf
Oscar Soto <oscarqpzm95@gmail.com>
2023-03-07 21:29:40 -0500

    i18nSelectPipe

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -3,9 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-no-comunes',
   templateUrl: './no-comunes.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class NoComunesComponent {
+  nombre: string = 'Adnilec';
+  genero: string = 'femenino';
 
+  invitacionMapa = {
+    masculino: 'invitarlo',
+    femenino: 'invitarla',
+    other: 'invitarte',
+  };
 }
