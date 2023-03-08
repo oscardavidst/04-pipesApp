ff26622c7b08c267b7b670c5c22f622c0b22fd1e
Oscar Soto <oscarqpzm95@gmail.com>
2023-03-08 13:20:00 -0500

    PrimeTable - llenar con data

--- src/app/ventas/pages/ordenar/ordenar.component.ts
+++ src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Color, Heroe } from '../../interfaces/ventas.interfaces';
 
 @Component({
   selector: 'app-ordenar',
@@ -7,6 +8,33 @@ import { Component } from '@angular/core';
 })
 export class OrdenarComponent {
   enMayusculas: boolean = false;
+  heroes: Heroe[] = [
+    {
+      nombre: 'Superman',
+      vuela: true,
+      color: Color.azul,
+    },
+    {
+      nombre: 'Batman',
+      vuela: false,
+      color: Color.negro,
+    },
+    {
+      nombre: 'Robin',
+      vuela: false,
+      color: Color.verde,
+    },
+    {
+      nombre: 'Daradevil',
+      vuela: false,
+      color: Color.rojo,
+    },
+    {
+      nombre: 'Linterna Verde',
+      vuela: true,
+      color: Color.verde,
+    },
+  ];
 
   toggleMayusculas() {
     this.enMayusculas = !this.enMayusculas;
