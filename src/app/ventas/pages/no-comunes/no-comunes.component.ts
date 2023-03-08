9394d36d23d4985bcd202ef5f20c08d8b1f243aa
Oscar Soto <oscarqpzm95@gmail.com>
2023-03-07 21:41:29 -0500

    i18nPlural Pipe

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -6,12 +6,20 @@ import { Component } from '@angular/core';
   styles: [],
 })
 export class NoComunesComponent {
+  //i18nSelect
   nombre: string = 'Adnilec';
   genero: string = 'femenino';
-
   invitacionMapa = {
     masculino: 'invitarlo',
     femenino: 'invitarla',
     other: 'invitarte',
+  };
+
+  //i18nPlural
+  clientes: string[] = ['Maria', 'Oscar'];
+  clientesMapa = {
+    '=0': 'no tenemos nigún cliente',
+    '=1': 'tenemos un cliente',
+    other: 'tenemos # clientes',
   };
 }
